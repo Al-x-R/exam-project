d00bd9daaa1f2e6a34ee6e309d39106770e7bec1
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-11-06 23:21:27 +0200

    Add basic structure and styles to forgot password form

--- client/src/components/forms/ForgotPasswordForm/index.js
+++ client/src/components/forms/ForgotPasswordForm/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import * as Yup from 'yup';
+import {ErrorMessage, Field, Form, Formik} from 'formik';
+import PropTypes from 'prop-types';
+import styles from './ForgotPasswordForm.module.sass'
 
-const ForgotPasswordForm = () => {
-    return (
-        <div>
-            
-        </div>
+const initialValues = {
+    email: '',
+    password: '',
+};
+
+const validationSchema = Yup.object({
+    email: Yup.string().trim().email().required(),
+    password: Yup.string().required(),
+});
+
+function ForgotPasswordForm(props) {
+    const { onSubmit } = props;
+
+    const handleSubmit = useCallback(
+        (values, formikBag) => {
+            onSubmit(values);
+        },
+        [onSubmit]
     );
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+        >
+            {() => (
+                <Form className={styles.form}>
+                    <div className={styles.field}>
+                        <Field name="email" className={styles.input}/>
+                        <ErrorMessage name="email" />
+                    </div>
+                    <div className={styles.field}>
+                        <Field name="password" className={styles.input}/>
+                        <ErrorMessage name="password" />
+                    </div>
+
+                    <button type="submit" className={styles.submit}>Get password</button>
+                </Form>
+            )}
+        </Formik>
+    );
+}
+
+ForgotPasswordForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
 };
 
 export default ForgotPasswordForm;
