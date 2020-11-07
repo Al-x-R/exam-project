db1d9ef648dc7d53c1ee1381f0fc3797c3428aac
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-11-07 17:14:20 +0200

    Move the get_in_touch section into a separate component

--- client/src/pages/HowItWork/HowItWork.js
+++ client/src/pages/HowItWork/HowItWork.js
@@ -5,14 +5,13 @@ import HowItWorkStep from '../../components/HowItWorkStep/HowItWorkStep';
 import Footer from '../../components/Footer/Footer';
 import data from './steps.json';
 import HowItWorkQuestionsList from '../../components/HowItWorkQuestionsList/HowItWorkQuestionsList';
-import Icon from '@mdi/react';
-import {mdiEmailOutline} from '@mdi/js';
 import video from './HowDoesSquadhelpWork.mp4';
+import GetInTouch from '../../components/GetInTouch/GetInTouch';
 
 const HowItWork = () => {
 
     return (
-        <>
+        <div className={styles.how_it_work_page}>
             <Header/>
             <div className={styles.container}>
                 <section className={styles.how_it_work}>
@@ -50,30 +49,9 @@ const HowItWork = () => {
                 </div>
                 <HowItWorkQuestionsList/>
             </div>
-            <section className={styles.get_in_touch}>
-                <div className={styles.container}>
-                    <div className={styles.get_in_touch_wrapper}>
-                        <div className={styles.icon}>
-                            <Icon path={mdiEmailOutline}
-                                  size={2}
-                                  color="#455a89"/>
-                        </div>
-                        <div className={styles.contacts}>
-                            <h1>Questions?</h1>
-                            <div className={styles.contacts_text}>Check out our <a href="#" target="_blank">FAQs</a>
-                                or send us a <a href="#" target="_blank">message</a>.
-                                For assistance with launching a contest, you can also call us at (877)&nbsp;355-3585 or
-                                schedule a <a href="#" target="_blank">Branding Consultation</a>
-                            </div>
-                        </div>
-                        <div className={styles.et_in_touch_btn}>
-                            <a href="#" target="_blank">Get in touch</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <GetInTouch/>
             <Footer/>
-        </>
+        </div>
     );
 };
 
