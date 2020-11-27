c47d7811ad8bd94c48c76a19f666534bca893ce2
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-11-27 15:19:00 +0200

    Add validation to events

--- client/src/components/Events/EventForm/EventForm.js
+++ client/src/components/Events/EventForm/EventForm.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
 import styles from './EventForm.module.sass';
 import {addEvent} from '../../../app/store/actions/eventsActionCreators';
 import {connect} from 'react-redux';
+import * as Yup from 'yup';
 
 const EventForm = (props) => {
   const {dispatch} = props;
+
+  const validationSchema = Yup.object({
+    title: Yup.string().trim().required(),
+    eventDate: Yup.date().min(new Date()).required(),
+  });
 
   return (
     <section className={styles.formContainer}>
@@ -15,6 +21,7 @@ const EventForm = (props) => {
           eventDate: Date.now(),
           startEvent: Date.now(),
         }}
+        validationSchema={validationSchema}
         onSubmit={(values, formikBag) => {
           dispatch(addEvent(values));
           formikBag.resetForm()
@@ -22,9 +29,11 @@ const EventForm = (props) => {
         <Form className={styles.form}>
           <div className={styles.field}>
             <Field name="title" className={styles.input} />
+            <ErrorMessage name="title" />
           </div>
           <div className={styles.field}>
             <Field name="eventDate" type="datetime-local" className={styles.input} />
+            <ErrorMessage name="eventDate" />
           </div>
           <button type="submit" className={styles.submit}>Create event</button>
         </Form>
