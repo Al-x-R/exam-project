34f7030279d5400faeed43ba3d72fe53332be835
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-01 14:35:03 +0200

    Add sort for events

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import EventItem from './EventItem/EventItem';
-import styles from './Events.module.sass'
+import styles from './Events.module.sass';
 
 const Events = () => {
   const { events } = useSelector(state => state.events);
 
+  const sortedEvents = [...events].sort((a, b) => a.eventDate > b.eventDate ? 1 : -1);
+
   return (
     <section>
-      <h2>EVENTS</h2>
-      <ul>
-        {events.map((event) => (
+      <ul className={styles.events}>
+        {sortedEvents.map((event) => (
           <li className={styles.eventItem} key={event.id}>
             <EventItem {...event} />
           </li>
