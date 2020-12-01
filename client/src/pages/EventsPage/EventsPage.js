4d49ec3c0e02402518ae1c943ed04b48d5d90d1f
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-01 14:52:31 +0200

    Add structure and styles to events page

--- client/src/pages/EventsPage/EventsPage.js
+++ client/src/pages/EventsPage/EventsPage.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import EventForm from '../../components/Events/EventForm/EventForm';
+import React, { useLayoutEffect, useState } from 'react';
 import Events from '../../components/Events/Events';
+import { Link, useLocation } from 'react-router-dom';
+import CONSTANTS from '../../constants';
+import styles from './EventsPage.module.sass';
+import { useSelector } from 'react-redux';
 
 const EventsPage = () => {
+  const [isEvents, setIsEvents] = useState();
+  const { events } = useSelector(state => state.events);
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    setIsEvents(location.pathname === '/events');
+  }, [location.pathname]);
+
+  const pageTitle = (isEvents && events.length === 0) ? 'You don\'t have any reminders' : 'Live upcoming checks';
 
   return (
-    <div>
-      <EventForm />
-      <Events />
-    </div>
+    <>
+      <section className={styles.main}>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <Link to='/'>
+              <img src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="Logo"/>
+            </Link>
+            <Link className={styles.createEventBtn} to='/new_reminder'>
+              Create a new reminder
+            </Link>
+          </header>
+          <h1 className={styles.pageTitle}>{pageTitle}</h1>
+          <Events/>
+        </div>
+      </section>
+    </>
   );
 };
 
